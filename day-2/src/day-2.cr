85f4dcea3348301de62061a41bf37f0a4ee2a087
Steven Polglase <swpolgla@pm.me>
2024-12-02 23:40:00 -0500

    Advent of Code 2024 Day 2 Part 2 Solution

--- day-2/src/day-2.cr
+++ day-2/src/day-2.cr
@@ -5,16 +5,37 @@ t1 = Time.monotonic
 file_contents = File.read("input/input.txt")
 # Split by line, remove empty strings, split lines by space, convert numbers in string format to ints
 reports = file_contents.split('\n').select { |x| x.size > 0 }.map { |x| x.split(' ').map { |y| y.to_i } }
+
+def asc_or_dec(list : Array(Int32))
+    list_sorted = list.sort
+    return list == list_sorted || list == list_sorted.reverse
+end
+
+def diff_check(list : Array(Int32))
+    return list.each_cons(2).sum { |x| (1..3).includes?((x[0] - x[1]).abs) ? 1 : 0 } == list.size - 1
+end
+
+# Part 1
 safe_count = 0
+kinda_safe_count = 0
+p1_reports = reports.clone
 reports.each do |report|
-    report_sorted = report.sort
-    if report != report_sorted && report != report_sorted.reverse
-        next
-    end
-    if report.each_cons(2).sum { |x| (1..3).includes?((x[0] - x[1]).abs) ? 1 : 0 } == report.size - 1
+    if asc_or_dec(report) && diff_check(report)
         safe_count += 1
+    else
+        # Part 2
+        report.each_with_index do |x, idx|
+            fake_report = report.clone
+            fake_report.delete_at(idx)
+            if asc_or_dec(fake_report) && diff_check(fake_report)
+                kinda_safe_count += 1
+                break
+            end
+        end
     end
 end
-puts safe_count
+puts "Completely Safe Reports: #{safe_count}"
+puts "Kinda Safe Reports: #{kinda_safe_count}"
+puts "Total 'Safe' Reports: #{safe_count + kinda_safe_count}"
 
 puts Time.monotonic - t1
